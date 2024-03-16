951eb7b5e70f83ff61d71404d0a6c354c01db9ba
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2024-03-16 22:07:55 +0100

    refactor: service subscriber

--- apps/frontend/apps/lyra-landing-page/astro.config.mjs
+++ apps/frontend/apps/lyra-landing-page/astro.config.mjs
@@ -1,7 +1,27 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
+import icon from 'astro-icon';
+import { loadEnv } from 'vite';
+import path from 'path';
+
+const DEV_PORT = 4321;
+const envDir = path.resolve(process.cwd() + '/../../../');
+console.log('🛸 envDir', envDir);
+const { BACKEND_URL } = loadEnv(process.env.NODE_ENV, envDir, '');
+
+console.log('🚀 BACKEND_URL', BACKEND_URL);
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [UnoCSS({ injectReset: true })],
+	site: import.meta.env.DEV ? `http://localhost:${DEV_PORT}` : 'https://lyra-nwhm.onrender.com',
+	integrations: [
+		UnoCSS({ injectReset: true }),
+		icon({
+			iconDir: 'src/assets/icons',
+			include: {
+				'simple-icons': ['*'],
+				ph: ['*'],
+			},
+		}),
+	],
 });

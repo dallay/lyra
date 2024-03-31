d5dbc07b5f549ed092a82eb0050db043c487a28b
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2024-03-31 11:17:13 +0200

    feat: [TASK-162] create a web component for an email subscription (#125)
    * feat: create new embeds web component app
    
    * chore(deps): update all dependencies #124
    
    * perf: 404 page
    
    * fix: merge vite config

--- apps/frontend/mock/vite.config.ts
+++ apps/frontend/mock/vite.config.ts
@@ -4,9 +4,8 @@ import fastify from 'vite-plugin-fastify';
 import fastifyRoutes from 'vite-plugin-fastify-routes';
 import { sharedViteConfig } from '@lyra/config/vite.config.shared';
 
-export default mergeConfig(
-	sharedViteConfig(__dirname),
-	defineConfig({
+export default defineConfig(
+	mergeConfig(sharedViteConfig(__dirname), {
 		define: envify({
 			HOST: process.env.HOST || '127.0.0.1',
 			PORT: process.env.PORT || 3000,
--- apps/frontend/packages/utilities/vite.config.ts
+++ apps/frontend/packages/utilities/vite.config.ts
@@ -1,11 +1,10 @@
-/** @type {import('vite').UserConfig} */
 import dts from 'vite-plugin-dts';
 import { defineConfig, mergeConfig } from 'vite';
 import { resolve } from 'path';
 import { sharedViteConfig } from '@lyra/config/vite.config.shared';
-export default mergeConfig(
-	sharedViteConfig(__dirname),
-	defineConfig({
+/** @type {import('vite').UserConfig} */
+export default defineConfig(
+	mergeConfig(sharedViteConfig(__dirname), {
 		build: {
 			lib: {
 				entry: resolve(__dirname, 'src/index.ts'),
@@ -14,6 +13,6 @@ export default mergeConfig(
 			},
 			target: 'esnext', // transpile as little as possible
 		},
-		plugins: [dts({ rollupTypes: true })], // emit TS declaration files
+		plugins: [dts({ rollupTypes: true })],
 	})
 );

dfa56fbe2ecf2f6f47523b611e72663ac44c5238
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2024-06-06 23:49:52 +0200

    fix: 🔴 Code scanning CodeQL

--- apps/backend/src/test/resources/db/workspace/clean.sql
+++ apps/backend/src/test/resources/db/workspace/clean.sql
@@ -1,4 +1,7 @@
 -- Delete all data from the workspace tables to ensure a clean state for testing.
--- The test data starts in `workspace_name` with "Test: ".
 
-DELETE FROM workspaces WHERE workspace_name LIKE 'Test: %';
+DELETE
+FROM workspaces
+WHERE workspace_id IN
+      ('a0654720-35dc-49d0-b508-1f7df5d915f1', 'a0654720-35dc-49d0-b508-1f7df5d915f1',
+       '894812b3-deb9-469f-b988-d8dfa5a1cf52', '949a8d91-1f53-4082-a4a9-7760fed234b0');

bf7968c388319a105c3d128dc2895f902cc791ea
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2024-08-17 21:50:31 +0200

    feat: integrate liquibase migration system

--- apps/backend/backend.gradle.kts
+++ apps/backend/backend.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation(libs.spring.boot.starter.data.r2dbc)
     implementation(libs.bundles.kotlin.jvm)
     implementation(libs.commons.text)
-//    implementation("org.liquibase:liquibase-core")
+    implementation("org.liquibase:liquibase-core")
     implementation("org.springframework:spring-r2dbc")
     implementation("org.springframework.data:spring-data-r2dbc")
     implementation("org.postgresql:r2dbc-postgresql")
--- apps/backend/src/test/kotlin/com/lyra/app/config/TestDatabaseConfiguration.kt
+++ apps/backend/src/test/kotlin/com/lyra/app/config/TestDatabaseConfiguration.kt
@@ -1,14 +1,9 @@
 package com.lyra.app.config
 
-import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
-import org.springframework.core.io.ClassPathResource
 import org.springframework.jdbc.datasource.DriverManagerDataSource
-import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
@@ -30,17 +25,5 @@ class TestDatabaseConfiguration {
         dataSource.password = postgres.password
 
         return dataSource
-    }
-
-    @Bean
-    fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
-        val initializer = ConnectionFactoryInitializer()
-        initializer.setConnectionFactory(connectionFactory)
-
-        val populator = CompositeDatabasePopulator()
-        populator.addPopulators(ResourceDatabasePopulator(ClassPathResource("db/schema-test.sql")))
-        initializer.setDatabasePopulator(populator)
-
-        return initializer
     }
 }

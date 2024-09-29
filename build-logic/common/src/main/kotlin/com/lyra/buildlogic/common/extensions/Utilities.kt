e820a9e184285986bc30f2dd49a34ccc24552e0a
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2024-09-30 00:09:27 +0200

    build(frontend): build logic to symbolic links env file

---
+++ build-logic/common/src/main/kotlin/com/lyra/buildlogic/common/extensions/Utilities.kt
@@ -0,0 +1,25 @@
+package com.lyra.buildlogic.common.extensions
+
+import java.nio.file.Files
+import org.gradle.api.Project
+
+/**
+ * Registers a Gradle task named "copyEnvFile" that creates symbolic links for the .env file
+ * in the specified locations.
+ *
+ * @param locations A list of relative paths where the .env file should be linked.
+ */
+fun Project.registerCopyEnvFileTask(locations: List<String>) {
+    tasks.register("copyEnvFile") {
+        doLast {
+            val rootEnvFile = project.rootDir.resolve(".env").toPath()
+            locations.forEach { location ->
+                val targetPath = project.rootDir.resolve(location).resolve(".env").toPath()
+                if (Files.exists(targetPath)) {
+                    Files.delete(targetPath)
+                }
+                Files.createSymbolicLink(targetPath, rootEnvFile)
+            }
+        }
+    }
+}

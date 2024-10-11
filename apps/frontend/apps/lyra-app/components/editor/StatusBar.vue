700f908ec7992965e8d66e4df812bc0d9414e69d
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2024-10-11 09:33:09 +0200

    refactor(editor): update StatusBar and UserAvatar components
    - Update StatusBar component to display draft and synced badges with a separator
    - Update UserAvatar component to accept class, size, and shape props

--- apps/frontend/apps/lyra-app/components/editor/StatusBar.vue
+++ apps/frontend/apps/lyra-app/components/editor/StatusBar.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="flex justify-between items-center mb-4">
     <div class="flex space-x-2">
-      <Badge variant="secondary">Draft</Badge>
-      <Badge variant="outline" class="bg-green-50 text-green-700">Synced</Badge>
+      <Badge variant="outline" class="flex items-center space-x-2">
+        <span class="text-muted-foreground">Draft</span>
+        <Separator orientation="vertical" class="mx-2" />
+        <span class="text-muted-foreground">Synced</span>
+      </Badge>
     </div>
-    <div class="flex space-x-2">
-      <UserAvatar />
+    <div class="flex justify-center items-center space-x-2">
+      <UserAvatar class="size-6"/>
       <Button variant="ghost" size="icon"><Undo class="h-4 w-4" /></Button>
       <Button variant="ghost" size="icon"><Redo class="h-4 w-4" /></Button>
       <Button variant="ghost" size="icon"><Search class="h-4 w-4" /></Button>
@@ -20,6 +23,7 @@
 
 <script setup lang="ts">
 import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
 import UserAvatar from '@/components/nav/UserAvatar.vue';
 import { Button } from '@/components/ui/button';
 import { Undo, Redo, Search, MessageSquare, Clock, Printer, Zap, Info } from 'lucide-vue-next';
--- apps/frontend/apps/lyra-app/components/nav/UserAvatar.vue
+++ apps/frontend/apps/lyra-app/components/nav/UserAvatar.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, type HTMLAttributes } from 'vue';
+import { cn } from '~/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage, type AvatarVariants, avatarVariant } from '@/components/ui/avatar';
 import { useAuthStore } from '~/store/auth.store';
 import type { IUser } from '@/domain/user';
 import { useUserAvatar } from '@/composables/useUserAvatar';
 const { getUser } = useAuthStore();
 const user = ref<IUser | null>(await getUser());
 const { userAvatar } = useUserAvatar(user.value);
+
+interface AvatarProps {
+  class?: HTMLAttributes['class']
+  size?: AvatarVariants['size']
+  shape?: AvatarVariants['shape']
+}
+const props = withDefaults(defineProps<AvatarProps>(), {
+  size: 'xs',
+  shape: 'circle',
+})
 </script>
 
 <template>
-  <Avatar class="h-8 w-8">
+  <Avatar :class="cn(avatarVariant({ size, shape }), props.class)">
     <AvatarImage :src="userAvatar" :alt="`@${user?.username}`" />
     <AvatarFallback>{{ initials(user?.name ?? "Y A P") }}</AvatarFallback>
   </Avatar>
